5d4837b56fbfe5f5c4f5958168360750bf180c3b
amudhavigneshwaran.p <amudhavigneshwaran.p@zohocorp.com>
2022-05-13 20:08:12 +0530

    structured folders

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import  React from 'react';
+import ReactDOM from 'react-dom'; //to hook entire react application  to index.html in public
+
+import App from './App';
+
+ReactDOM.render( <App />, document.getElementById('root'));
