bfb16ba9fea859b349841435e0f7852b9f720a90
amudhavigneshwaran.p <amudhavigneshwaran.p@zohocorp.com>
2022-05-14 20:37:53 +0530

    All components done

--- README.old.md
+++ README.old.md
@@ -1,1 +1,7 @@
 #inspired from https://github.com/adrianhajdin/project_modern_ui_ux_gpt3
+
+#gradient creation
+angrytools.com/gradient
+
+#custom css animation
+animista.net
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import  React from 'react';
 import ReactDOM from 'react-dom'; //to hook entire react application  to index.html in public
 
 import App from './App';
+import './index.css';
 
 ReactDOM.render( <App />, document.getElementById('root'));
